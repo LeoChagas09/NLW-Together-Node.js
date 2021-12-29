2291359dd532f835b77a4bb3cf51b843ff049429
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2021-12-29 16:57:38 -0300

    Update project files

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,8 +1,15 @@
 import { Request, Response, NextFunction } from "express";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+import { getCustomRepository } from "typeorm";
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+    const { user_id } = request;
+
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const { admin } = await usersRepositories.findOne(user_id);
+    
     // Verificar se usuario admin
-    const admin = true;
 
     if(admin) {
         return next();
--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-import { verify } from "jsonwebtoken"
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+    sub: string;
+}
 
 export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
     
@@ -15,14 +19,13 @@ export function ensureAuthenticated(request: Request, response: Response, next: 
 
     try {
         // Validar se token é valido
-        const decode = verify(token, "5a944ad990c08a29ce2432b09b75a6a3");
+        const { sub } = verify(token, "5a944ad990c08a29ce2432b09b75a6a3") as IPayload;
+        
+        // Recuperar informações do usuario
+        request.user_id = sub;
 
          return next();
     } catch (err) {
         return response.status(401).end();
     }
-    // Recuperar informações do usuario
-
-   
-
 }
